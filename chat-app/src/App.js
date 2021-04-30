a7ade7c43e8a63957c06f803587e377774e345b5
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-04-30 23:38:02 +0530

    done some changes

---
+++ chat-app/src/App.js
@@ -0,0 +1,95 @@
+import React from "react";
+import './App.css';
+
+import firebase from "firebase/app";
+import  "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAryU9D3LvsOPERBn9bYZqKjkLeD3aYLVk",
+  authDomain: "chat-app-fa8ac.firebaseapp.com",
+  databaseURL: "https://chat-app-fa8ac-default-rtdb.firebaseio.com",
+  projectId: "chat-app-fa8ac",
+  storageBucket: "chat-app-fa8ac.appspot.com",
+  messagingSenderId: "679020995868",
+  appId: "1:679020995868:web:512259bdbcf13a7f2686d9",
+  measurementId: "G-NH4Z7D7N6V"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const SignInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={SignInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <form>
+        <input value={formValue} onChange={} />
+
+        <button type="submit">🌫</button>
+      </form>
+    </>
+  )
+}
+function ChatMessage(props){
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return ( 
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL}/>
+      <p>{text}</p>
+    </div>
+
+  )
+}
+
+
+
+export default App;
