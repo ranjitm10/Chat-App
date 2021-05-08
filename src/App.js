99afc5e1319d712f12658029d15c7947d993b60a
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-05-08 20:13:43 +0530

    done some changes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1></h1>
+        <h1>Chattify</h1>
         <SignOut />
       </header>
 
@@ -54,7 +54,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>If we find your content doesn’t follow our policies You will get block</p>
     </>
   )
 
@@ -106,7 +106,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Type a message....." />
 
-      <button type="submit" disabled={!formValue}>⏏️Send</button>
+      <button type="submit" disabled={!formValue}>⏩</button>
 
     </form>
   </>)
